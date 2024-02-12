6570d80c5dcdcaaaf64515a6c1c04ffbe6e4fa27
Pablo <pablo.carvalho16@hotmail.com>
2024-02-12 11:43:39 -0300

    Enrriquecendo o modelo de domínio

--- CleanArchMvc.Domain/Entities/Category.cs
+++ CleanArchMvc.Domain/Entities/Category.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CleanArchMvc.Domain.Validation;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,11 +7,38 @@ using System.Threading.Tasks;
 
 namespace CleanArchMvc.Domain.Entities
 {
-    public class Category
+    public sealed class Category : EntityBase
     {
-        public int Id { get; set; }
-        public string Name { get; set; }
+        public string Name { get; private set; }
+
+        public Category(string name)
+        {
+            ValidadeDomain(name);
+        }
+
+        public Category(int id, string name)
+        {
+            DomainExceptionValidation.When(id < 0, "Invalid Id value!");
+            Id = id;
+            ValidadeDomain(name);
+        }
+
+        public void Update(string name)
+        {
+            ValidadeDomain(name);
+        }
 
         public ICollection<Product> Products { get; set; }
+
+        private void ValidadeDomain(string name)
+        {
+            DomainExceptionValidation.When(string.IsNullOrEmpty(name), 
+                "Invalid name. Name is required!");
+
+            DomainExceptionValidation.When(name.Length < 3,
+                "Invalid name. Too short, minimum 3 characters!");
+
+            Name = name;
+        }
     }
 }
