8997a75c47693644cf36f72ec5a2dfe8bd3cd42f
Pablo <pablo.carvalho16@hotmail.com>
2024-02-12 18:58:23 -0300

    Ajustes no modelo de domínio Product

--- CleanArchMvc.Domain.Tests/ProductUnitTest1.cs
+++ CleanArchMvc.Domain.Tests/ProductUnitTest1.cs
@@ -48,6 +48,13 @@ namespace CleanArchMvc.Domain.Tests
         }
 
         [Fact]
+        public void CreateProduct_WithNullImageName_NoNullReferenceException()
+        {
+            Action action = () => new Product(1, "Product Name", "Product Description", 9.99m, 99, null);
+            action.Should().NotThrow<NullReferenceException>();
+        }
+
+        [Fact]
         public void CreateProduct_WithEmptyImageName_NoDomainExeption()
         {
             Action action = () => new Product(1, "Product Name", "Product Description", 9.99m, 99, "Product Image");
