3299332c8e894154c15410b7efa95d94b48f4f51
pablokz1 <pablo.carvalho16@hotmail.com>
2024-03-22 15:41:24 -0300

    Implementado Commands e Queries para Product - CQRS

---
+++ CleanArchMvc.Application/Products/Commands/ProductRemoveCommand.cs
@@ -0,0 +1,14 @@
+﻿using CleanArchMvc.Domain.Entities;
+using MediatR;
+
+namespace CleanArchMvc.Application.Products.Commands
+{
+    internal class ProductRemoveCommand : IRequest<Product>
+    {
+        public int Id { get; set; }
+        public ProductRemoveCommand(int id)
+        {
+            Id = id;
+        }
+    }
+}
